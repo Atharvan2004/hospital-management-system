0eb1667542706277d9fc9c478c995f0a1be552ec
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2024-03-18 22:56:55 +0530

    pages folder and some pages routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/Login/LoginPage";
 import "./core-ui/App.css";
+import About from "./pages/About";
+import Policy from "./pages/Policy";
+import HomePage from "./pages/HomePage";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Hello</h1>} />
+        <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<LoginPage/>} />
+        <Route path="/policy" element={<Policy/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/about" element={<About/>} />
       </Routes>
     </BrowserRouter>
   );
