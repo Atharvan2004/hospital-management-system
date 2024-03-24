f32faa8457c18b577d352016a0e2218f435b8b93
Atharvan <atharvanpohnerkar2@gmail.com>
2024-03-24 23:58:36 +0530

    feat: appointment model

---
+++ server/models/appointment.js
@@ -0,0 +1,49 @@
+import { Schema, model } from "mongoose";
+
+const appointmentSchema = new Schema({
+  patientID: {
+    type: Schema.Types.ObjectId,
+    ref: "Patient",
+  },
+  contact: {
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+  },
+  doctorID: {
+    type: Schema.Types.ObjectId,
+    ref: "Doctor",
+  },
+  time: String,
+  status: {
+    type: String,
+    default: "Pending",
+  },
+});
+
+const Appointment = model("Appointment", appointmentSchema);
+
+// const sampleappointment = {
+//   patientID: "60f7890c6d9f1d40385c4d7a", // Sample patient ID
+//   contact: {
+//     phone: "1234567890", // Sample phone number
+//     email: "example@example.com", // Sample email
+//   },
+//   doctorID: "60f7890c6d9f1d40385c4d7b", // Sample doctor ID
+//   time: "2024-03-24T10:00:00", // Sample appointment time
+//   status: "Pending", // Default status
+// };
+
+// Appointment.insertMany(sampleappointment)
+//       .then((result) => {
+//           console.log('Documents inserted:', result);
+//       })
+//       .catch((error) => {
+//           console.error('Error inserting documents:', error);
+//       });
+
+export default Appointment;
