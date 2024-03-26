35be886308f5574c16bc542d3b646259fc5bbfc3
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2024-03-27 03:01:29 +0530

    some change

--- client/src/store/reducer.js
+++ client/src/store/reducer.js
@@ -2,12 +2,14 @@ import { combineReducers } from "@reduxjs/toolkit";
 import doctorSlice from "./slices/doctorSlice";
 import patientSlice from "./slices/patientSlice";
 import staffSlice from "./slices/staffSlice";
+import { resultSlice } from "./slices/searchResult";
 
 
 const rootReducer=combineReducers({
     doctor:doctorSlice,
     patient:patientSlice,
-    staff:staffSlice
+    staff:staffSlice,
+    result:resultSlice
     
 });
 
