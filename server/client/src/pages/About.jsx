95f84c78147e0cdd0a67803e1ea518b0120684c6
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2024-04-17 19:13:47 +0530

    fixes footer in about page and set correct margins

--- server/client/src/pages/About.jsx
+++ server/client/src/pages/About.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const About = () => {
   return (
     <>
-        <div className="flex mt-40 ml-40 mr-80">
+    <div className="flex mt-80 ml-96 mr-70 mb-72">
       <div className="">
         <img
           src="/src/assets/contact.avif" // corrected the image source path
@@ -16,12 +16,13 @@ const About = () => {
 
         <div className="text-xl font-semibold mb-4">About Us</div> {/* Added text-xl, font-semibold, and mb-4 for styling */}
         <p className="text-justify mt-2 max-w-md"> {/* Changed w-30 to max-w-md and adjusted mt-12 to mt-2 */}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-          officiis obcaecati esse tempore unde ratione, eveniet mollitia,
-          perferendis eius temporibus dicta blanditiis doloremque explicabo
-          quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
-          accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
-          commodi illum quidem neque tempora nam.
+        Welcome to Medilink, where your health and well-being are our top priorities.
+
+Our dedicated team is here to provide you with the highest quality care and support,
+
+ensuring that you receive personalized treatment to your needs.
+
+We look forward to serving you and helping you achieve your health goals."
         </p>
       </div>
     </div>
